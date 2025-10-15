94d4ee1ef41031a038fdf5efe7ae8f11687fa104
Likheng Yeo <2023342yeo@aupp.edu.kh>
2025-10-15 13:34:17 +0700

    Project setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
 import { Button } from '@/components/ui/button';
+import { SignInButton, SignedIn, SignedOut, SignOutButton } from "@clerk/nextjs";
 export default function Home() {
   return (
-    <>
-      <Button>Click me</Button>
-    </>
+    <div>
+      <h1>Home page</h1>
+      <SignedOut>
+        <SignInButton mode="modal">Sign Up</SignInButton>
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton>Logout</SignOutButton>
+      </SignedIn>
+    </div>
   );
 }
